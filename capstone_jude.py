5dee7251a8f6bebe9673db809539740a47b8147e
21nujood40 <82135699+21nujood40@users.noreply.github.com>
2021-04-18 20:51:25 +0900

    Add files via upload

--- capstone_jude.py
+++ capstone_jude.py
@@ -11,39 +11,33 @@ wn.bgcolor("light blue")
 wn.setup(800, 600)
 wn.tracer(0)
 
-#create the player
-#player = turtle.Turtle()
-#player.color("brown")
-#player.shape("turtle")
-#player.speed(0)
-#player.penup() #remove line
-#player.setheading(90) #direction of pic
-#player.dx = 0
-
-
-class Player():
+class Mole():    
     def __init__(self, row, col):
         self.row = row
         self.col = col
         self.position = "standing"
         
-    def go_right():
-        pass
-    def go_left():
-        pass
-    def go_forward():
-        pass
-    def go_backwards():
-        pass
+    def go_right(self):
+        self.col += 1
 
+    def go_left(self):
+        self.col -= 1
+
+    def go_forward(self):
+        self.row += 1
+
+    def go_backwards(self):
+        self.row -= 1
+
+mole = Mole(1,1)
 #Keyboard Bindings
 wn.listen() 
-wn.onkeypress(Player(go_right), "Right")
-wn.onkeypress(go_left, "Left")
-wn.onkeypress(go_forward, "Down")
-wn.onkeypress(go_backwards, "Up")
-        
-player = Player(1,1)
+wn.onkeypress(mole.go_right, "Right")
+wn.onkeypress(mole.go_left, "Left")
+wn.onkeypress(mole.go_forward, "Down")
+wn.onkeypress(mole.go_backwards, "Up")
+   
+win = (7,4) 
 
 #Create grid
 grid = [
@@ -58,7 +52,31 @@ grid = [
 for row in range(len(grid)):
     for col in range(len(grid[row])):
         print(grid[row][col], end="")
-    print("")
+    print("") 
 
+#def stopwatch(t):
+    #while t:
+        #mins, secs = divmod(t, 60)
+        #timer = '{:02d}:{:02d}'.format(mins, secs)
+        #print(timer, end = "\r")
+        #time.sleep(1)
+        #t -= 1
+        #print ("goodbye")
+        
+while True:
+    print(mole.row, mole.col)
+    
+    # Is the mole at the winning point
+    if mole.row == 7 and mole.col == 4:
+        print("Win")
+    # Check if the mole is off the grid
+    if mole.row ==5 and mole.col == 9 or mole.row == 3 and mole.col <= 0 or mole.row == 4 and mole.col <= 4 or mole.row == 5 and mole.col <= 5 or mole.col == 3 and mole.row <= 0 or mole.col == 4 and mole.row <= 0 or mole.col == 5 and mole.row <= 0 or mole.col == 6 and mole.row <= 1 or mole.col == 7 and mole.row <= 1 or mole.col == 8 and mole.row <= 1 or mole.col == 9 and mole.row <= 2:
+        print("Lose")
+    
+    #stopwatch(5)
+    
+
+    
+    wn.update()
 
 wn.mainloop()
